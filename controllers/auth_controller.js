a7738f6ca1b38486a870d2038a083a9d0819f830
karimelghazouly <karimelghazouly@gmail.com>
2019-03-06 12:27:42 +0200

    Add authentication controller

---
+++ controllers/auth_controller.js
@@ -0,0 +1,25 @@
+var bodyParser = require('body-parser');
+var urlparser = bodyParser.urlencoded({extended : false});
+var usermodel = require('../models/user_model');
+var userdata = require('../userdata');
+
+module.exports = function(app)
+{
+    app.get('/register', urlparser, function(req, resp){
+        resp.render('Pages/register',{userid: userdata.userid});
+    });
+
+    app.get('/login', urlparser, function(req, resp){
+        resp.render('Pages/login',{userid: userdata.userid});
+    });
+
+    app.post('/login', urlparser, function(req, resp){
+        data = req.body;
+        usermodel.LoginUser(resp, data);
+    });
+
+    app.post('/register', urlparser, function(req, resp){
+        data = req.body;
+        usermodel.SaveUser(resp, data);
+    });
+}
